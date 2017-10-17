cc91205e55891ba2ce22a02c443083e805de451d
pekrismer <krismerpeter96@live.com>
2017-10-17 23:03:50 +0200

    tets code entfernt

--- src/main/java/businessLogic/Main.java
+++ src/main/java/businessLogic/Main.java
@@ -11,16 +11,6 @@ public class Main {
 		WarehouseManagement testManagement = new WarehouseManagement(testWarehouse);
 		
 		
-		/*testManagement.extendWarehouse(1, 0);
-		testManagement.addArticle(new Article("Artikel 9", "9", "100er Packung", "obi", 400));
-		for (Integer string : testManagement.getPosition("9")) {
-			System.out.println(string);
-		}
-		System.out.println(testManagement.Inventar());
-		
-		CLI test = new CLI(args, testManagement);
-		test.parse();*/
-		
 		CLI test = new CLI(testManagement);
 		test.start();
 	}
